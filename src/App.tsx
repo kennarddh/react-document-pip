b49ac2737c4b2ecd8a60bb7acd0a45d8b5b8f2fb
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-11-14 23:52:52 +0700

    feat: Simple component for PIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
+
+import ReactDocumentPIP, {
+	ReactDocumentPIPHandle,
+} from 'Components/ReactDocumentPIP'
 
 const App: FC = () => {
-	return <div></div>
+	const ReactDocumentPIPRef = useRef<ReactDocumentPIPHandle>(null)
+
+	return (
+		<div>
+			<h1>Document Picture In Picture</h1>
+			<button
+				onClick={() =>
+					console.log(
+						'Open PIP with handle',
+						ReactDocumentPIPRef.current?.Open(),
+					)
+				}
+			>
+				Open!
+			</button>
+			<button
+				onClick={() =>
+					console.log(
+						'Close PIP with handle',
+						ReactDocumentPIPRef.current?.Close(),
+					)
+				}
+			>
+				Close!
+			</button>
+			<ReactDocumentPIP
+				ref={ReactDocumentPIPRef}
+				onOpen={event => console.log('Open PIP', event)}
+				onClose={event => console.log('Close PIP', event)}
+			>
+				<h2>It&apos;s real!!!</h2>
+			</ReactDocumentPIP>
+		</div>
+	)
 }
 
 export default App
---
+++ src/Components/ReactDocumentPIP.ts
@@ -0,0 +1,85 @@
+import {
+	ReactNode,
+	forwardRef,
+	useCallback,
+	useEffect,
+	useImperativeHandle,
+	useState,
+} from 'react'
+
+import { createPortal } from 'react-dom'
+
+export interface ReactDocumentPIPProps {
+	children: ReactNode
+	onClose?: (event: PageTransitionEvent) => void
+	onOpen?: (event: DocumentPictureInPictureEvent) => void
+	allowReopen?: boolean
+}
+
+export interface ReactDocumentPIPHandle {
+	Open: () => Promise<boolean>
+	Close: () => boolean
+}
+
+const ReactDocumentPIP = forwardRef<
+	ReactDocumentPIPHandle,
+	ReactDocumentPIPProps
+>(function ReactDocumentPIP(
+	{ children, onClose, onOpen, allowReopen = false },
+	ref,
+) {
+	const [Container, SetContainer] = useState<HTMLElement | null>(null)
+
+	useEffect(() => {
+		const eventHandler = (event: DocumentPictureInPictureEvent) => {
+			onOpen?.(event)
+		}
+
+		window.documentPictureInPicture?.addEventListener('enter', eventHandler)
+
+		return () =>
+			window.documentPictureInPicture?.removeEventListener(
+				'enter',
+				eventHandler,
+			)
+	}, [onOpen])
+
+	const Open = useCallback(async () => {
+		if (window.documentPictureInPicture?.window && !allowReopen)
+			return false
+
+		const pipWindow = await window.documentPictureInPicture?.requestWindow({
+			width: 200,
+			height: 200,
+		})
+
+		if (!pipWindow) return false
+
+		pipWindow?.addEventListener('pagehide', event => {
+			onClose?.(event)
+		})
+
+		SetContainer(pipWindow?.document.body ?? null)
+
+		return true
+	}, [allowReopen, onClose])
+
+	const Close = useCallback(() => {
+		if (!window.documentPictureInPicture) return false
+
+		window.documentPictureInPicture?.window?.close()
+
+		SetContainer(null)
+
+		return true
+	}, [])
+
+	useImperativeHandle(ref, () => ({
+		Open,
+		Close,
+	}))
+
+	return Container ? createPortal(children, Container) : null
+})
+
+export default ReactDocumentPIP
